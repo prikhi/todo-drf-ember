f4eb201d457943e5c5c9fc17299d55084d79e03a
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-05-07 04:05:39 -0400

    Cleanup Model

--- client/app/models/task.js
+++ client/app/models/task.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  //id: DS.attr('number'),
   title: DS.attr('string'),
   description: DS.attr('string'),
   favorite: DS.attr('boolean'),
